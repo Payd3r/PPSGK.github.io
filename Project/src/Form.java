f17f0cb91de1871aca98f866e062af7997e917c7
FilippoPellegata <filippopellegata@gmail.com>
2021-04-21 14:37:32 +0200

    PROVA

--- Project/src/Form.java
+++ Project/src/Form.java
@@ -16,7 +16,7 @@ public class Form extends javax.swing.JFrame {
     public Form() {
         initComponents();
     }
-
+//SUCAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
