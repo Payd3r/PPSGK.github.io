baff69933cff685cb695c0ec02f69459bd35cb08
galeandro03 <galegabry1@gmail.com>
2021-05-18 17:59:04 +0200

    tolto
    aggiustato

--- DietaSmart/src/dietasmart/Frame.java
+++ DietaSmart/src/dietasmart/Frame.java
@@ -597,13 +597,7 @@ public class Frame extends javax.swing.JFrame {
             riga=lettore.readLine();
             file.close();
         }
-        SerialPort serialPort = new SerialPort("/dev/ttyUSB0");// For windows "COMX" (e.g : "COM1") should works
-            if (serialPort.openPort()) {
-                serialPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_ODD);
-        byte[] buffer = serialPort.readBytes(1);// Read one byte
-        String str = new String(buffer);
-        serialPort.closePort();
-}
+    
 
         
       
